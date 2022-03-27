ed4d6796177ac0b27ca63ef1bfba9fbddddefae3
david <david0305mc@gmail.com>
2022-03-27 09:09:46 +0900

    Add MainUI

---
+++ Assets/Scripts/UI/UIMain.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UnityEngine.UI;
+
+public class UIMain : MonoBehaviour
+{
+    [SerializeField] private Button shopBtn;
+
+    private void Start()
+    {
+        InitUI();
+    }
+
+    public void InitUI()
+    {
+        shopBtn.onClick.AsObservable().Subscribe(_ =>
+        {
+            Debug.Log("shopBtn subscribe");
+        });
+    }
+}
